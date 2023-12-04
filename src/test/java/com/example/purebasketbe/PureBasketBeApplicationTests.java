11e63b427fd2363532d895511a028180ec5f27fc
Sanghyu Lee <hyouoo@gmail.com>
2023-12-05 00:52:34 +0900

    Fix: 테스트코드 수정

--- src/test/java/com/example/purebasketbe/PureBasketBeApplicationTests.java
+++ src/test/java/com/example/purebasketbe/PureBasketBeApplicationTests.java
@@ -1,16 +1,12 @@
 package com.example.purebasketbe;
 
-import com.example.purebasketbe.domain.product.entity.Event;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 class PureBasketBeApplicationTests {
 
     @Test
     void contextLoads() {
-        Event event = Event.DISCOUNT;
 
-        System.out.println(event.rate(0.50));
     }
 
 }
