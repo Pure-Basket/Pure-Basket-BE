11fc06ddc94a1ba20620e80f5504756b60b2f26b
Sanghyu Lee <hyouoo@gmail.com>
2023-12-08 01:38:37 +0900

    Feat: Cart domain 수정 [#3]

--- src/main/java/com/example/purebasketbe/domain/cart/CartRepository.java
+++ src/main/java/com/example/purebasketbe/domain/cart/CartRepository.java
@@ -1,10 +1,8 @@
 package com.example.purebasketbe.domain.cart;
 
 import com.example.purebasketbe.domain.cart.entity.Cart;
-import com.example.purebasketbe.domain.product.entity.Product;
 import com.example.purebasketbe.domain.member.entity.Member;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.example.purebasketbe.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,15 +13,18 @@ import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository <Cart,Long> {
-    Optional<Cart> findByProductId(Long productId);
-    Page<Cart> findAll(Pageable pageable);
+
     @Modifying
     @Query("DELETE FROM Cart c WHERE c.member=:member AND c.product IN (:products)")
     void deleteByUserAndProductIn(Member member, List<Product> products);
 
-    @Query("SELECT p FROM Cart p JOIN FETCH p.product WHERE p.member = :member")
+    @Query("SELECT p.id, p.name, p.price, p.category, c.amount FROM Cart c " +
+            "JOIN FETCH c.product p " +
+            "WHERE c.member = :member")
     List<Cart> findAllByMember(Member member);
 
-
     void deleteAllByMemberAndProductIn(Member member, List<Product> productList);
+
+    Optional<Cart> findByProductIdAndMember(Long productId, Member member);
+
 }
--- src/main/java/com/example/purebasketbe/domain/product/ImageRepository.java
+++ src/main/java/com/example/purebasketbe/domain/product/ImageRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
@@ -12,4 +13,7 @@ public interface ImageRepository extends JpaRepository<Image, Long> {
     void deleteAllByProductId(Long productId);
 
     List<Image> findAllByProductId(Long productId);
+
+    Optional<Image> findByProductId(Long productId);
+
 }
